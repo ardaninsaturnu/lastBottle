0912a14f340ec8178267410ae16c783d71190548
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2023-02-25 22:50:53 +0300

    gam board passing the earth function is updated. @development

--- src/Components/GameBoard/index.js
+++ src/Components/GameBoard/index.js
@@ -1,26 +1,50 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import Square from "../../Elements/Square";
 import Henry from "../../Elements/Henry";
 import Bottle from "../../Elements/Bottle";
 import Gpgp from "../../Elements/Gpgp";
 import './board.css';
 
+const directions = ["north", "northeast", "east", "southeast", "south", "southwest", "west", "northwest"];
+
 const GameBoard = ({ dice }) => {
   const board = [];
+  const [ rowCount, setRowCount ] = useState({henryRow: 8, bottleRow: 2, patchRow: 5});
+  const [ columnCount, setColumnCount ] = useState({henryColumn: 42, bottleColumn: 8, patchColumn: 5});
+  const [ prevPlayer, setPrevPlayer ] = useState( 'bottle');
   
-  useEffect(()=> {
-    if( dice )
-      console.log(dice)
+  useMemo(() => {
+    if( dice ){
+      const currentPlayerRow = prevPlayer === 'bottle' ? 'bottleRow' : 'henryRow';
+      const currentPlayerColumn = prevPlayer === 'bottle' ? 'bottleColumn' : 'henryColumn';
+  
+      console.log(currentPlayerColumn,currentPlayerRow)
+      
+      setRowCount( prev => ({
+        ...rowCount,
+        [ currentPlayerRow ]: ( prev[currentPlayerRow] + dice.step ) > 24 ? ( prev[currentPlayerRow] + dice.step - 24 ) : prev[currentPlayerRow] + dice.step
+    }))
+  
+      setColumnCount( prev => ({
+        ...columnCount,
+        [currentPlayerColumn]: ( prev[currentPlayerColumn] + dice.step ) > 42 ? ( prev[currentPlayerColumn] + dice.step -42 ) : prev[currentPlayerColumn] + dice.step
+      }))
+  
+      prevPlayer === 'bottle' ? setPrevPlayer('henry') : setPrevPlayer('bottle');
+    }
+    
   },[dice])
+  
+  console.log( rowCount, columnCount, 'culha' )
   
   for ( let r = 1; r <= 24; r++ ) {
     const row = [];
     for ( let c = 1; c <= 42; c++ ) {
       row.push(
         <Square key={`${r}-${c}`}>
-          { r === 2 && c === 8 ? <Henry/> : null }
-          { r === 5 && c === 42 ? <Bottle/> : null }
-          { r === 12 && c === 12 ? <Gpgp/> : null }
+          { r === rowCount.henryRow && c === columnCount.henryColumn  ? <Henry/> : null }
+          { r === rowCount.bottleRow && c === columnCount.bottleColumn ? <Bottle/> : null }
+          { r === rowCount.patchRow && c === columnCount.patchColumn ? <Gpgp/> : null }
         </Square>
       );
     }
@@ -39,6 +63,7 @@ const GameBoard = ({ dice }) => {
       <div key={`c${j}`} className="column-header columnHeader">{`C${j + 1}`}</div>
     );
   }
+  
   return (
     <div className="board">
       <div className="column-header columnHeader"/>
--- src/Components/RollDice/index.js
+++ src/Components/RollDice/index.js
@@ -1,40 +1,48 @@
-import React, {useEffect, useState} from "react";
-import {constant} from "../../helpers/constant";
-import {removeValue} from "../../helpers/utility";
+import React, {useState} from "react";
+
+const oppositeDirections = {
+  "north": "south",
+  "northeast": "southwest",
+  "east": "west",
+  "southeast": "northwest",
+  "south": "north",
+  "southwest": "northeast",
+  "west": "east",
+  "northwest": "southeast",
+};
 
 function DiceRoller({ dice, setDice }) {
-  const [directionName, setDirectionName] = useState( 'Mustafa' )
-  const [ previousPlayer, setPreviousPlayer ] = useState(1)
-  
-  console.log('constant bug', constant)
-  
-  const specifyDirection = ( dice, previousPlayer,constant ) => {
-    const newConstant = [
-      ...constant.slice(0, previousPlayer),
-      ...constant.slice(previousPlayer + 1)
-        .concat(
-          constant[previousPlayer].opposite.map((i) => constant[i - 1])
-        )
-    ]
-      .slice(0, 8);
-    console.log(newConstant)
-  }
+  const [ directions, setDirections ] = useState( [ "north", "northeast", "east", "southeast", "south", "southwest", "west", "northwest" ]);
   
   function rollDice() {
     const diceDirection = Math.floor(Math.random() * 6) + 1;
     const diceStep = Math.floor(Math.random() * 6) + 1;
-    setDice({direction: diceDirection, step: diceStep });
+    let directionState = directions;
+    
+    // Get the index of the first player's direction
+    const firstPlayerDirection = directions[diceDirection];
+
+// Remove the first player's direction and its opposite from the array
+    const forbiddenDirections = [firstPlayerDirection, oppositeDirections[firstPlayerDirection]];
+    directions.splice(diceDirection, 1);
+    for (const forbiddenDirection of forbiddenDirections) {
+      const index = directions.indexOf(forbiddenDirection);
+      if (index !== -1) {
+        directions.splice(index, 1);
+      }
+    }
+    
+    setDirections(directions)
+    
+    setDice({ ...dice, direction: diceDirection, step: diceStep, directions  });
+    setDice({ ...dice, direction: diceDirection, step: diceStep });
   }
-  
-  useEffect(()=> {
-    specifyDirection(dice, previousPlayer,constant)
-  },[dice])
   
   console.log('roll dice', dice)
   
   return (
     <div className="dice-roller">
-      <div className="dice-content"> Direction <div className="dice-content-box">{ dice?.direction }</div><strong>{directionName}</strong></div>
+      <div className="dice-content"> Direction <div className="dice-content-box">{ dice?.direction }</div><strong>{'North'}</strong></div>
       <div className="dice-content">Steps <div style={{padding:6,border:'2px black solid',width:24,height:24,display:"flex",justifyContent:'center',alignItems:'center'}}>{ dice?.step }</div></div>
       <button onClick={rollDice} className="button button-dice">Zar At</button>
     </div>
